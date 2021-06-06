4cd4e7648caacd9f4d5104e313e3a064c06f04fb
Leo Battaglia <ms@raptorizm.com>
2021-06-06 06:49:51 +0200

    Version 0.0.3

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+//Constants
 var selina = require("selina");
 var server = new selina.Server(false, 0, response);
+var p = server.getPrompt();
+//Start Server
 server.init();
+server.startListener();
 server.start();
+//Functions
 function response(req) {
     return "RESPONSE";
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
+//Constants
 const selina = require("selina");
-let server = new selina.Server(false, 0, response);
+const server = new selina.Server(false, 0, response);
+const p = server.getPrompt();
+
+//Start Server
 server.init();
+server.startListener();
 server.start();
 
+//Functions
 function response(req:Request):string{
     return "RESPONSE";
 }
