834e983994979cceb1f1d0481f3be5cbc553383e
Leo Battaglia <ms@raptorizm.com>
2021-09-10 15:39:20 +0200

    Version 0.0.10

--- lib/builder.ts
+++ lib/builder.ts
@@ -18,85 +18,79 @@ let uo:URLObject;
 //Functions
 function buildBody():HTMLSourceCode{
     let sc = new HTMLSourceCode();
-    let tag:HTMLElement = sc.openTag("body");
-    tag.addStyle("display", "flex");
-    tag.addStyle("flex-direction", "row");
-    tag.addStyle("height", "100%");
-    tag.addStyle("margin", "0em");
-    tag.addStyle("padding", "0em");
-    tag.addStyle("width", "100%");
-    tag = sc.openTag("div");
-    tag.addStyle("height", "100%");
-    tag.addStyle("width", "100%");
+    let tag:HTMLElement = sc.openBodyDefault();
+    tag.addStyleDisplayFlexRow();
+    tag = sc.openTagWithID("div", "div_body");
+    tag.addStyleSizes("100%", "100%");
 
     //TODO: Load Page-Content
 
-    sc.closeTag("div");
+    sc.closeDiv();
     if(uo.containsPara("admin", "1")){
-        tag = sc.openTag("div");
+        tag = sc.openDiv();
         //tag.addAttribute("class", "font-effect-neon")
-        tag.addStyle("background-color", "#334c81");
-        tag.addStyle("display", "flex");
-        tag.addStyle("flex-direction", "column");
-        tag.addStyle("height", "calc(100% - 2em)");
-        tag.addStyle("padding", "1em");
-        tag.addStyle("width", "18em");
-        tag = sc.addTag("img");
+        tag.addStyleBackgroundColor("#334c81");
+        tag.addStyleDisplayFlexColumn();
+        tag.addStyleSizes("18em", "calc(100% - 2em)");
+        tag.addStylePadding("1em");
+        tag = sc.addImgDefault("Alessia.svg", "Alessia-Framework");
+        tag.addStyleWidth("100%");
+        tag = sc.openForm();
+        tag.addAttributeAction("-");
+        tag = sc.openLabel();
+
+        tag.addAttribute("for", "email");
+        sc.addContent("Mail")
+        tag = sc.closeTag("label");
+        tag = sc.addTag("input");
+        tag.addAttribute("type", "text");
+        tag.addAttribute("id", "email");
+        tag.addAttribute("name", "email");
         tag.addStyle("width", "100%");
-        tag.addAttribute("src", "Alessia.svg");
 
         //TODO: Finish Admin-Panel
 
-        sc.closeTag("div");
+        sc.closeForm();
+        sc.closeDiv();
     }else if(uo.containsPara("admin", "0")){
         mode_admin = false;
     }
-    sc.closeTag("body");
+    sc.closeBody();
     return sc;
 }
 
 function buildMeta():HTMLSourceCode{
     let sc = new HTMLSourceCode();
     !sys.isNull(config.charset)? sc.addCharset(config.charset): undefined;
-    !sys.isNull(config.author)? sc.addMeta("author", config.author): undefined;
-    !sys.isNull(config.company)? sc.addMeta("company", config.company): undefined;
-    !sys.isNull(config.content_language)? sc.addMeta("content-language", config.content_language): undefined;
-    !sys.isNull(config.copyright)? sc.addMeta("copyright", config.copyright): undefined;
-    !sys.isNull(config.description)? sc.addMeta("description", config.description): undefined;
-    !sys.isNull(config.google_site_verification)? sc.addMeta("google-site-verification", config.google_site_verification): undefined;
-    !sys.isNull(config.keywords)? sc.addMeta("keywords", config.keywords): undefined;
-    !sys.isNull(config.page_topic)? sc.addMeta("page-topic", config.page_topic): undefined;
-    !sys.isNull(config.publisher)? sc.addMeta("publisher", config.publisher): undefined;
-    !sys.isNull(config.revisit_after)? sc.addMeta("revisit-after", config.revisit_after): undefined;
-    !sys.isNull(config.robots)? sc.addMeta("robots", config.robots): undefined;
-    !sys.isNull(config.viewport)? sc.addMeta("viewport", config.viewport): undefined;
+    !sys.isNull(config.author)? sc.addMetaDefault("author", config.author): undefined;
+    !sys.isNull(config.company)? sc.addMetaDefault("company", config.company): undefined;
+    !sys.isNull(config.content_language)? sc.addMetaDefault("content-language", config.content_language): undefined;
+    !sys.isNull(config.copyright)? sc.addMetaDefault("copyright", config.copyright): undefined;
+    !sys.isNull(config.description)? sc.addMetaDefault("description", config.description): undefined;
+    !sys.isNull(config.google_site_verification)? sc.addMetaDefault("google-site-verification", config.google_site_verification): undefined;
+    !sys.isNull(config.keywords)? sc.addMetaDefault("keywords", config.keywords): undefined;
+    !sys.isNull(config.page_topic)? sc.addMetaDefault("page-topic", config.page_topic): undefined;
+    !sys.isNull(config.publisher)? sc.addMetaDefault("publisher", config.publisher): undefined;
+    !sys.isNull(config.revisit_after)? sc.addMetaDefault("revisit-after", config.revisit_after): undefined;
+    !sys.isNull(config.robots)? sc.addMetaDefault("robots", config.robots): undefined;
+    !sys.isNull(config.viewport)? sc.addMetaDefault("viewport", config.viewport): undefined;
     return sc;
 }
 
 function buildPage():Response{
     sc = new HTMLSourceCode();
     sc.addDoctype();
-    let tag:HTMLElement = sc.openTag("html");
-    tag.addStyle("height", "100%");
-    tag.addStyle("width", "100%");
-    sc.openTag("head");
-    sc.addSourceCode(buildTitle());
+    sc.openHTMLDefault();
+    sc.openHead();
+    sc.addTitle(config.title);
     sc.addSourceCode(buildMeta());
     //sc.openTag("style");
     //sc.addContent("@font-face{font-family: \"Alessia\"; src: url(\"/Apalu.ttf\");}")
     //sc.closeTag("style");
-    sc.closeTag("head");
+    sc.closeHead();
     sc.addSourceCode(buildBody());
-    sc.closeTag("html")
+    sc.closeHtml()
     return new Response(200, "text/html", sc.getHTML());
-}
-
-function buildTitle():HTMLSourceCode{
-    let sc = new HTMLSourceCode();
-    sc.openTag("title");
-    sc.addContent(config.title)
-    sc.closeTag("title");
-    return sc;
 }
 
 function getContentType(path:string):string{
@@ -115,9 +109,6 @@ function getFile():Response{
     uo.page.substr(0, 1) === "/"? slash = "": slash="/";
     let path:string = "./lib/res" + slash + uo.page;
     return new Response(200, getContentType(path), fs.readFileSync(path));
-
-    //TODO: All
-
 }
 
 export function response(req:Request):Response{
